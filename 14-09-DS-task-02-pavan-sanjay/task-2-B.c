c31336b1c3a6d3583477987027959f19f6c8222d
rockaroll <84375063+rockaroll@users.noreply.github.com>
2021-09-16 11:57:23 +0530

    Update task-2-B.c

--- 14-09-DS-task-02-pavan-sanjay/task-2-B.c
+++ 14-09-DS-task-02-pavan-sanjay/task-2-B.c
@@ -2,11 +2,16 @@
 #include<stdlib.h>
 #pragma warning(disable:4996)
 int main() {
+	//declaration of pointers and variables
 	int* ptr, n, i, s, x,*sig;
+	
+	//inputting size and creating array using calloc
+	
 	printf("input size of array:");
 	scanf("%d", &n);
 	printf("input elements:");
 	ptr = (int*)calloc(5,sizeof(n));
+	//inputting data and printing
 	for (i = 0; i < n; i++) {
 		scanf("%d", ptr + i);
 	}
@@ -15,9 +20,13 @@ int main() {
 		printf("%d ", *(ptr + i));
 	}
 	printf("\n");
+	//switch case menu asking if you want to reallocate or free space
+	
 	printf("||do you want to reallocate or free space ||\n||press 1 for reallocation||\n|| 2 for freeing space.||");
 	scanf("%d", &s);
 	if (s == 1) {
+		//reallocation taking place
+		
 		printf("new size:");
 		scanf("%d", &x);
 		
@@ -26,6 +35,8 @@ int main() {
 			ptr = sig;
 		}
 		printf("reallocation sucessful\n");
+		//inputting and printing elements for reallocated space
+		
 		printf("input elements:");
 		for (i = 0; i < x; i++) {
 			scanf("%d", (ptr + i));
@@ -39,6 +50,7 @@ int main() {
 	}
 	else
 		if (s == 2) {
+			//freeing up memory for using it
 			free(ptr);
 			printf("freeing up space has been successful\n");
 	}

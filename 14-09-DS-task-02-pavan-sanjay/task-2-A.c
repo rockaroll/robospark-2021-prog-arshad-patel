a26a1e88461132d30e8ab24ea31dfcb5ebe2d81b
rockaroll <84375063+rockaroll@users.noreply.github.com>
2021-09-16 11:46:30 +0530

    Update task-2-A.c

--- 14-09-DS-task-02-pavan-sanjay/task-2-A.c
+++ 14-09-DS-task-02-pavan-sanjay/task-2-A.c
@@ -1,15 +1,19 @@
 #include<stdio.h>
 #pragma warning(disable:4996)
+//declaration of structure for bank account
 struct account {
     int number;
     char name[100];
     int blcamount;
 };
+//function declarations
 void balance(struct  account a[],int size,int n);
 void deposit(struct  account a[], int size, int n,int s);
 void withdrawal(struct  account a[], int size, int n, int s);
 void loop();
 int main() {
+    /* this was to allow the user to use ou facilities continously without disruption
+     * if they choose to us our facilities they press 1 if not then they press 0*/
     int s;
     printf("||WELCOME TO THE BANK||\nIF YOU WOULD LIKE TO USE OUR FACILITIES: press 1\n TO QUIT: press 0 ");
     scanf("%d", &s);
@@ -23,6 +27,7 @@ int main() {
         }
 }
 void loop() {
+    /*This function is where the main input of details happens.It asks you how many records you want to insert and displays it.keep in mind the maximum space is 10 records*/
     struct account b[10];
     int i, n, choice, j=0;
         printf("enter no of records  to input:");
@@ -45,7 +50,9 @@ void loop() {
         for (i = 1; i <= n; i++) {
             printf("%7d \t %s %7d\n", b[i].number, b[i].name, b[i].blcamount);
         }
-
+        /*this is the switch menu here you have three options to choose from:1-to check the accounts with balance<100
+         *2-to deposit money into the bank
+         *3-to withdraw money from the bank */
         int s;
         printf("***        pick your choice       ***");
         printf("\n||1 -  accounts with balance < 100||\n|| 2 - to deposit a fixed amount ||\n|| 3 - to withdraw a fixed amount||");
@@ -70,7 +77,7 @@ void loop() {
 }
 void balance(struct account a[],int size, int n) {
     int i, swap = 0;
-    
+    /*to check the  account of balance<100*/
     for (i = 1; i <= n; i++) {
         
         if (a[i].blcamount < 100) {
@@ -84,7 +91,8 @@ void balance(struct account a[],int size, int n) {
    
 }
 void deposit(struct  account a[], int size, int n, int s) {
-
+    /*to deposit a fixed amount*/
+     /*it will print name,account number,updated amount,amount deposited*/
     int i=0, k;
     printf("how much should be deposited?");
     scanf("%d", &k);
@@ -93,9 +101,11 @@ void deposit(struct  account a[], int size, int n, int s) {
    printf("account number:%d\n", a[s].number);
    printf(" updated amount:%d\n", a[s].blcamount);
    printf("amount deposited:%d\n", k);
-  
+ 
 }
 void withdrawal(struct  account a[], int size, int n, int s) {
+    /*to withdraw a fixed amount*/
+    /*it will print name,account number,updated amount,amount deposited*/
     int i, k;
     if (a[s].blcamount <= 1) {
         printf("transaction is not possible\n");

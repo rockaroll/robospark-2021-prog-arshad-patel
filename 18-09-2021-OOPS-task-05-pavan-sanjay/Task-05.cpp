a89057d4bf3cd95526e28fdfce898ce4a5f584f0
rockaroll <84375063+rockaroll@users.noreply.github.com>
2021-09-22 00:05:00 +0530

    Update Task-05.cpp
    this is the commented version of task-05 from trf robospark .The question was deal with different types of constructors.

--- 18-09-2021-OOPS-task-05-pavan-sanjay/Task-05.cpp
+++ 18-09-2021-OOPS-task-05-pavan-sanjay/Task-05.cpp
@@ -3,24 +3,27 @@
 #include<bits/stdc++.h>
 #include<conio.h>
 using namespace std;
+//this is the class rectangle with one default constructor and two parameterised constructors
 class rectangle{
     public:
     int length,breadth;
-    
+    //default constructor
     rectangle(){
         length=0;
         breadth=0;
     }
+    //single value parameterised constructor
     rectangle(int a){
         length=a;
         breadth=a;
     }
+    //two valued parameterised constructor
     rectangle(int a,int b){
         
         length=a;
         breadth=b;
     }
-
+   //set functions for length,breadth and height
     void setlength(int inputl){
       length=inputl;
     }
@@ -28,6 +31,7 @@ class rectangle{
 
       breadth=inputb;
     }
+    //get functions to return values
    int getlength(){
         return length;
     }
@@ -45,17 +49,20 @@ class rectangle{
 int main(){
   int h;
   rectangle b;
+  //menu to implement different constructors  
   cout<<"what kind of consturctor do you want to implement?\n";
   cout<<"1-default constructor\n";
   cout<<"2-parameterised constructor with one parameter\n";
   cout<<"3-parameterised constructor with two parameters\n";
   cout<<"4-set parameters through directly through set functions\n";
   cin>>h;
+  //default constructor option 
   if(h==1){
   cout<<"length="<<b.getlength()<<endl;
   cout<<"breadth="<<b.getbreadth()<<endl;;
   cout<<"area="<<b.getarea()<<endl;
   }
+  //single value constructor option
   else if(h==2){
    int x;
    cout<<"enter value of both length and breadth";
@@ -65,6 +72,7 @@ int main(){
   cout<<"breadth="<<b.getbreadth()<<endl;;
   cout<<"area="<<b.getarea()<<endl;
   }
+  //multiple value constructor option
   else if(h==3){
       int s,v;
     cout<<"enter values of both length and breadth";
@@ -74,6 +82,7 @@ int main(){
   cout<<"breadth="<<b.getbreadth()<<endl;;
   cout<<"area="<<b.getarea()<<endl;
   }
+   // input through set functions
   else if(h==4){
       int x,y;
       cout<<"input length and breadth";

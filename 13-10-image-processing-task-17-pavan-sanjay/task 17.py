8ff8bb1cef07ff26911126f409136fd1736c6fbf
pavan <pks10802@gmail.com>
2021-10-14 00:16:50 +0530

    task-17-submission

---
+++ 13-10-image-processing-task-17-pavan-sanjay/task 17.py
@@ -0,0 +1,46 @@
+#here importing packages
+import numpy as np
+import cv2
+import math
+#here image is getting read
+img = cv2.imread('shapes.png')
+
+imgGry = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+ret , thrash = cv2.threshold(imgGry, 240 , 255, cv2.CHAIN_APPROX_NONE)
+contours , hierarchy = cv2.findContours(thrash, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+
+
+#used  to draw lines on figure
+for contour in contours:
+    approx = cv2.approxPolyDP(contour, 0.01* cv2.arcLength(contour, True), True)
+    cv2.drawContours(img, [approx], 0, (0, 0, 0), 5)
+    x = approx.ravel()[0]
+    y = approx.ravel()[1] - 5
+    #to identify beach shape
+    if len(approx) == 3:
+        cv2.putText( img, "Triangle", (x, y), cv2.FONT_HERSHEY_COMPLEX, 0.5, (0, 0, 0) )
+    elif len(approx) == 4 :
+        x, y , w, h = cv2.boundingRect(approx)
+        aspectRatio = float(w)/h
+        print(aspectRatio)
+        if aspectRatio >= 0.95 and aspectRatio < 1.05:
+            cv2.putText(img, "square", (x, y), cv2.FONT_HERSHEY_COMPLEX, 0.5, (0, 0, 0))
+
+        else:
+            cv2.putText(img, "rectangle", (x, y), cv2.FONT_HERSHEY_COMPLEX, 0.5, (0, 0, 0))
+
+    elif len(approx) == 5 :
+        cv2.putText(img, "pentagon", (x, y), cv2.FONT_HERSHEY_COMPLEX, 0.5, (0, 0, 0))
+    elif len(approx) == 10 :
+        cv2.putText(img, "star", (x, y), cv2.FONT_HERSHEY_COMPLEX, 0.5, (0, 0, 0))
+    else:
+      if math.pow(x,2)+math.pow(y,2)==1:
+        cv2.putText(img, "circle", (x, y), cv2.FONT_HERSHEY_COMPLEX, 0.5, (0, 0, 0))
+      else:
+          cv2.putText(img, "elipse", (x, y), cv2.FONT_HERSHEY_COMPLEX, 0.5, (0, 0, 0))
+
+#ouputs
+cv2.imshow('shapes', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()

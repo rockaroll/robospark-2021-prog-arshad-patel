9bd02bb4090a64792b956e9f449c8762fc084df6
pavan <pks10802@gmail.com>
2021-10-16 22:53:19 +0530

    task-17-submission-with-ss

--- 13-10-image-processing-task-17-pavan-sanjay/task 17.py
+++ 13-10-image-processing-task-17-pavan-sanjay/task 17.py
@@ -10,7 +10,6 @@ imgGry = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 ret , thrash = cv2.threshold(imgGry, 240 , 255, cv2.CHAIN_APPROX_NONE)
 contours , hierarchy = cv2.findContours(thrash, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
 
-
 #used  to draw lines on figure
 for contour in contours:
     approx = cv2.approxPolyDP(contour, 0.01* cv2.arcLength(contour, True), True)
